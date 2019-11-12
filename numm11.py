50f733aa71677319991797dd70ceb268ad172052
pineapple79 <fun.goul.109@gmail.com>
2019-11-12 21:16:02 +0600

    Task1-11

---
+++ numm11.py
@@ -0,0 +1,8 @@
+string = input("Enter your string here")
+string = string.strip()
+search = (" ")
+spaces = string.count(search)
+word_count = str(spaces +1)
+print(("The number of words in your string: ") + word_count)
+
+# перевести в лист и посчитать кол-во слов
